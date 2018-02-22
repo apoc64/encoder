04fc1a0f20beff4cf5fad8d8d3e09a718039b5dd
Steve Schwedt <sschwedt@gmail.com>
2018-02-22 13:55:24 -0700

    Added encrypt/decrypt file methods

--- encryptor.rb
+++ encryptor.rb
@@ -26,4 +26,28 @@ class Encryptor
      encrypt(string, -rotation)
    end
 
+   def encrypt_file(filename, rotation, encrypting = true)
+     # Create file handle to the input file
+     input_file = File.open(filename, "r")
+     # read text of file
+     message_to_encrypt = input_file.read
+     # Create name for output file
+     if !encrypting
+       output_filename = filename.gsub("encrypted", "decrypted")
+     else
+       output_filename = "#{filename}.encrypted"\
+     end 
+     output_file = File.open(output_filename, "w")
+     # Create output file handle
+     # Write out the text
+     output_file.write(encrypt(message_to_encrypt, rotation))
+     # Close the file
+     input_file.close
+     output_file.close
+   end
+
+   def decrypt_file(filename, rotation)
+     encrypt_file(filename, -rotation, false)
+   end
+
 end
